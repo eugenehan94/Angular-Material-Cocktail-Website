511ffbeec6fa06d65b732952f7b74b6784580a9b
eugenehan94 <eugene.han94@gmail.com>
2022-11-02 19:17:03 -0400

    added to footer - message that differs per screen size

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent implements OnInit {
-
   year: any;
+  getScreenWidth: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.year = new Date().getFullYear();
+    this.getScreenWidth = window.innerWidth;
   }
 
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+  }
 }
