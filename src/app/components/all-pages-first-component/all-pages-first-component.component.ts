a1c9772a946763f7d175ef9e9822ba675bf2cbba
eugenehan94 <eugene.han94@gmail.com>
2022-11-30 21:13:43 -0500

    added comments for future reference

--- src/app/components/all-pages-first-component/all-pages-first-component.component.ts
+++ src/app/components/all-pages-first-component/all-pages-first-component.component.ts
@@ -1,23 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-all-pages-first-component',
   templateUrl: './all-pages-first-component.component.html',
   styleUrls: ['./all-pages-first-component.component.css'],
 })
 export class AllPagesFirstComponentComponent implements OnInit {
+  // @Input() indicates the value is pass down from parent component.
   @Input() cardSubtitle?: string;
   @Input() cardContent: any;
-  @Input() cardContentError: any;
+  @Input() cardContentError?: boolean;
   @Input() cardContentLoaded?: boolean;
-  constructor(private router: Router) {}
+
+  constructor() {}
 
   ngOnInit(): void {
-    // router.url returns /url - remove the /, then remove %20(this occurs when there is a space)
-    // const routerUrl = this.router.url.substring(1).replace(/%20/, ' ');
-    // then capitalize the first letter and attach the rest
-    // const title = routerUrl.charAt(0).toUpperCase() + routerUrl.slice(1);
     this.cardSubtitle = this.cardSubtitle?.toUpperCase();
- 
   }
 }
